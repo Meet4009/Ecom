c5baef54addaaa915b73b50edbd37e6b681904d9
Meet4009 <meetsolanki4009@gmail.com>
2025-04-12 12:49:58 +0530

    small Changes

--- app.js
+++ app.js
@@ -28,10 +28,6 @@ app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/admin", userRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// home Route
-app.use("/", (req,res)=>{
-    res.send("Everithig is Fine")
-})
 // Error Middleware
 app.use(errorMiddleware);
 
